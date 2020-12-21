0e2a07a3f8b0df6f75dca2b4ef28a5a7a05725e2
unknown <robertodm501@gmail.com>
2020-12-22 00:59:06 +0100

    ejercicio hecho

--- scripts/recu.js
+++ scripts/recu.js
@@ -1,13 +1,31 @@
 window.addEventListener("load", () => {
-
+    document.getElementById("literatura").addEventListener("change",escogerInfantil)
 }, false);
 
 function escogerInfantil(){
-    if(literatura == infantil){
-
+    console.log("entrq");
+    let literatura = document.getElementById("literatura");
+    let divInfantil= document.getElementById("infantil");
+    console.log(literatura.value);
+    if(literatura.value == "juvenil"){
+        console.log("eantra if")
+        divInfantil.setAttribute("hidden","");
+        divInfantil.setAttribute("disabled","");
+    }
+    else if(literatura.value == "adulta"){
+        divInfantil.setAttribute("hidden","");
+        divInfantil.setAttribute("disabled","");
+    }
+    else{
+        divInfantil.removeAttribute("hidden");
+        divInfantil.removeAttribute("disabled");
     }
 }
 
 function nuevoLibro(){
-    
+
+}
+
+function avisos(){
+
 }
