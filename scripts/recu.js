022bc29ab1e9f12cf124d93f43748fb557a1667f
unknown <robertodm501@gmail.com>
2020-12-22 02:46:39 +0100

    ejercicio hecho

--- scripts/recu.js
+++ scripts/recu.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-    document.getElementById("literatura").addEventListener("change",escogerInfantil)
+    document.getElementById("literatura").addEventListener("change",escogerInfantil);
 }, false);
 
 function escogerInfantil(){
@@ -22,10 +22,29 @@ function escogerInfantil(){
     }
 }
 
+function imprimirFormulario(){
+    let select = document.getElementById("literatura");
+    let div = document.getElementById("nombre");
+
+    for(i = 0; i ;i++){
+        if(select){
+
+        }
+    }
+}
+
 function nuevoLibro(){
 
 }
 
 function avisos(){
-
+    if(document.getElementById("time") >= "9:00" || document.getElementById("time") <= "19:00"){
+        alert("Sección de alta abierta");
+    }
+    else if(document.getElementById("time") <= "9:00" || document.getElementById("time") >= "19:00"){
+        alert("Está fuera del horario. Solo es posible dar de alta libros de lunes a viernes de 9:00 a 19:00");
+    }
+    else{
+        alert("En días festivos no es posible dar de alta nuevos libros");
+    }
 }
