c3327b34998eacb5186d9348b6202794beb935d1
unknown <robertodm501@gmail.com>
2020-12-22 12:20:59 +0100

    ejercicio hecho

--- scripts/recu.js
+++ scripts/recu.js
@@ -1,8 +1,14 @@
 window.addEventListener("load", () => {
     document.getElementById("literatura").addEventListener("change",escogerInfantil);
     document.getElementById("seccionLibros").addEventListener("imprimir", imprimirFormulario, false);
+    avisos();
+    imprimirFormulario();
 }, false);
 
+/**
+ * Si se trata de literatura infantil, aparecerán además los siguientes campos(cuyos datos se
+almacenarán solo en este caso). (Edad recomendada y observaciones)
+ */
 function escogerInfantil(){
     console.log("entrq");
     let literatura = document.getElementById("literatura");
@@ -22,30 +28,67 @@ function escogerInfantil(){
         divInfantil.removeAttribute("disabled");
     }
 }
-
+/**
+ * A la derecha del formulario habrá otra sección que permitirá ver el nombre de todos los libros
+de un determinado tipo ordenados por año de publicación
+ */
+/**
+ * Creo Array para guardar libros
+ */
+var ArrayParaLibros = [];
 function imprimirFormulario(){
-    let select = document.getElementById("literatura");
-    let div = document.getElementById("nombre");
 
-    for(i = 0; i ;i++){
-        if(select){
+    let select = document.getElementById("literatura"); 
+    let informacion = document.getElementById("seccionLibros");
 
-        }
-    }
+    informacion.innerHTML = "";
+    informacion.innerHTML = "Los libros de tipo "+select.value;
+
+
+   for (let index = 0; index < ArrayParaLibros.length; index++) {
+       if (select.value == ArrayParaLibros[index].Literatura) {
+        informacion.innerHTML = ArrayParaLibros[index].Nombre+ArrayParaLibros[index].NumeroCopias;
+        
+       }
+       if (select.value == "vacio") {
+        informacion.innerHTML = ArrayParaLibros[index].Nombre;           
+       }
+   }   
 }
 
 function nuevoLibro(){
 
 }
-
+/**
+ * En esta sección se incluirá un aviso en rojo, centrado y un tamaño de letras superior a los
+demás textos de la página. El aviso dependerá del día y hora actual y ha de comprobarse
+con la fecha real
+ */
 function avisos(){
-    if(document.getElementById("time") >= "9:00" || document.getElementById("time") <= "19:00"){
-        alert("Sección de alta abierta");
+    let divAvisos = document.getElementById("seccionAviso");
+    let dia = new Date().getDay();
+    let hora = new Date().getHours();
+    let minuto = new Date().getMinutes();
+    let boton = document.getElementById("enviar");
+
+    /**
+     * Si es sábado o domingo 
+     */
+    if(dia == 6 || dia == 0){
+        divAvisos.innerHTML = "En días festivos no es posible dar de alta nuevos libros";
+        boton.disabled = true;
     }
-    else if(document.getElementById("time") <= "9:00" || document.getElementById("time") >= "19:00"){
-        alert("Está fuera del horario. Solo es posible dar de alta libros de lunes a viernes de 9:00 a 19:00");
+    /**
+     * Si es un día de lunes a viernes y la hora está entre las 9:00 y las 19:00
+     */
+    else if((dia >= 1 && dia <= 5) && (hora >= 9 && hora <= 19) && (minuto >= 0)){
+        divAvisos.innerHTML = "Está fuera del horario. Solo es posible dar de alta libros de lunes a viernes de 9:00 a 19:00"
     }
+    /**
+     * Si es un día de lunes a viernes pero fuera del horario anterior
+     */
     else{
-        alert("En días festivos no es posible dar de alta nuevos libros");
+        divAvisos.innerHTML = "En días festivos no es posible dar de alta nuevos libros";
+        boton.disabled = true;
     }
 }
